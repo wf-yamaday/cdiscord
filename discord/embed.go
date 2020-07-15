2e249aa5712bf3d2c5d9a49257d01abe9c059993
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-07-15 20:19:28 +0900

    feat: Discordのwebhookのリクエストを送る構造体の定義
    参照:https://birdie0.github.io/discord-webhooks-guide/structure/embeds.html

---
+++ discord/embed.go
@@ -0,0 +1,11 @@
+﻿package discord
+
+type EmbedParam struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Color       string `json:"color"`
+}
+
+type Embeds struct {
+	Embeds []EmbedParam `json:"embeds"`
+}
